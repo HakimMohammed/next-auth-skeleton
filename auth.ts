575c60ae6e920398f8f8599a96ae25670cf7a366
Mohamed Hakim <mohamed.hakim.dev@gmail.com>
2026-03-06 16:20:45 +0000

    configure keycloak provider

--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Keycloak from "next-auth/providers/keycloak";
 
 export const {auth, handlers, signIn, signOut} = NextAuth({
-    providers: [GitHub],
+    providers: [GitHub, Keycloak],
 });
