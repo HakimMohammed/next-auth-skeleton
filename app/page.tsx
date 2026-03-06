e16c46a15c7e2ac2f6d542202924a4f44109014c
Mohamed Hakim <mohamed.hakim.dev@gmail.com>
2026-03-06 15:02:48 +0000

    refacotr logout button

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-import { auth } from "@/auth";
 import LoginForm from "@/features/auth/components/login-form";
-import Link from "next/link";
 
-export default async function Home() {
-    const session = await auth();
-
-    if (session?.user) {
-        return (
-            <div>
-                <Link href="/profile">Go to Profile</Link>
-            </div>
-        );
-    }
-
+export default function Page() {
     return <LoginForm />;
 }
--- features/auth/components/sing-out-button.tsx
+++ features/auth/components/sing-out-button.tsx
@@ -1,6 +1,17 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { logout } from "@/features/auth/actions";
+import { LogOut } from "lucide-react";
 
 export const SignOutButton = () => {
-    return <button onClick={() => logout()}>Sign out</button>;
+    return (
+        <Button
+            variant="outline"
+            className="border border-destructive text-destructive hover:bg-destructive/10 hover:text-destructive cursor-pointer"
+            onClick={() => logout()}
+        >
+            <LogOut className="mr-2 h-4 w-4" />
+            Sign out
+        </Button>
+    );
 };
