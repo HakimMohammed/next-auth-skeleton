9567d3cc518c425fed65456f3f82014053822b3b
Mohamed Hakim <mohamed.hakim.dev@gmail.com>
2026-03-06 16:20:19 +0000

    login with github and keycloak actions

--- features/auth/actions.ts
+++ features/auth/actions.ts
@@ -1,11 +1,17 @@
 "use server";
 import { signIn, signOut } from "@/auth";
 
-export const login = async () => {
+export const loginGitHub = async () => {
     await signIn("github", {
         redirectTo: "/profile",
     });
 };
+
+export const loginKeycloak = async () => {
+    await signIn("keycloak", {
+        redirectTo: "/profile",
+    });
+}
 
 export const logout = async () => {
     await signOut({ redirectTo: "/" });
