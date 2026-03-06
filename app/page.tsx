8a9895a63f73b42f3d51787d1819fca0648d6ba0
Mohamed Hakim <mohamed.hakim.dev@gmail.com>
2026-03-06 13:48:20 +0000

    basic info display

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
-"use client";
+import { auth } from "@/auth";
+import { SignInButton } from "@/features/auth/components/sing-in-button";
+import Image from "next/image";
 
-import { login } from "@/features/auth/actions";
+export default async function Home() {
+    const session = await auth();
 
-export default function Home() {
+    if (session?.user) {
+        return (
+            <div>
+                <h1>Auth Skeleton with Next Auth and GitHub</h1>
+                <p>You are signed in as {session.user.name}.</p>
+                <p>{session.user.email}</p>
+                <Image src={session.user.image || ""} alt="User Avatar" width={100} height={100} />
+            </div>
+        );
+    }
+
     return (
         <div>
+            <h1>Auth Skeleton with Next Auth and GitHub</h1>
             <p>You are not signed in.</p>
-            <button onClick={() => login()}>Sign in with GitHub</button>
+            <SignInButton />
         </div>
     );
 }
